096517655a8b5a6397d7d7527258890bc22cf6ff
Ahmad Adha <adhaahmad04@gmail.com>
2025-02-18 13:40:12 +0800

    test

---
+++ components/footer.tsx
@@ -0,0 +1,9 @@
+const Footer = () => {
+  return (
+    <footer className="border-t">
+      <div className="container mx-auto py-10">Footer</div>
+    </footer>
+  );
+};
+
+export default Footer;
---
+++ components/header.tsx
@@ -0,0 +1,10 @@
+import { DockDemo } from "./header-dock";
+const Header = () => {
+  return (
+    <header className="w-full mx-auto fixed top-0 z-50">
+      <DockDemo />
+    </header>
+  );
+};
+
+export default Header;
